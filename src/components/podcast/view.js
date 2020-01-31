6a617b787e29e51b2edc9b8f69d65aeb76011652
yosefkurniawan <jojo@icube.us>
2020-01-31 16:44:15 +0700

    apply data for dynamic content

--- src/components/podcast/view.js
+++ src/components/podcast/view.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ContextPodcasts from '../../data/contextPodcasts.js';
 import Episodes from '../../components/podcast/episodes.js';
+import { useParams } from "react-router-dom";
 
 function View() {
     const backButtonText = '<< Kembali';
-    const episodeId = 2;
+    const { podcastid } = useParams();
     function getThumbnail(podcast) {
         if(podcast) {
             return podcast['thumbnail'];
@@ -28,17 +29,18 @@ function View() {
     }
     return (
         <div className="podcast-view">
+
             <ContextPodcasts.Consumer>
                 {(context) => (
                     <>
                         <div className="podcast-image-wrapper">
-                            <img src={getThumbnail(context.getPodcastById(episodeId))} alt={getTitle(context.getPodcastById(2))} style={{'width': '100%'}}/>
+                            <img src={getThumbnail(context.getPodcastById(podcastid))} alt={getTitle(context.getPodcastById(podcastid))} style={{'width': '100%'}}/>
                             <a href="# " className="button" style={{'marginTop':'20px'}}>{backButtonText}</a>
                         </div>
                         <div className="podcast-info-wrapper">
-                            <h3 className="podcast-title">{getTitle(context.getPodcastById(episodeId))}</h3>
-                            <p className="podcast-desc"><a href={getUrl(context.getPodcastById(episodeId))}>{getUrl(context.getPodcastById(episodeId))}</a></p>
-                            <Episodes podcast={context.getPodcastById(episodeId)} />
+                            <h3 className="podcast-title">{getTitle(context.getPodcastById(podcastid))}</h3>
+                            <p className="podcast-desc"><a href={getUrl(context.getPodcastById(podcastid))}>{getUrl(context.getPodcastById(podcastid))}</a></p>
+                            <Episodes podcast={context.getPodcastById(podcastid)} />
                         </div>
                     </>
                 )}
