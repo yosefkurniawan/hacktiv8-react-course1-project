08b515fd1dbd789e6346fbb6de9f8d709a9afd4b
yosefkurniawan <jojo@icube.us>
2020-02-03 14:00:03 +0700

    apply lazy and suspense on loading header dan content

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import Header from './components/page/header.js';
-import Content from './components/page/content.js';
-import DataProvider from "./data/dataProvider.js";
+import React, { lazy, Suspense } from 'react';
+import DataProvider from '././data/dataProvider.js';
 import './App.css';
+
+const Header = lazy(() => import('./components/page/header.js'));
+const Content = lazy(() => import('./components/page/content.js'));
 
 class App extends React.Component {
   render() {
     return (
       <div className="app">
         <DataProvider>
-          <Header />
-          <Content />
+          <div className="header-wrapper">
+            <Suspense fallback={<div className="spinner">Loading Header</div>}>
+              <Header />
+            </Suspense>
+          </div>
+          <div className="content-wrapper">
+            <Suspense fallback={<div className="spinner">Loading Content</div>}>
+              <Content />
+            </Suspense>
+          </div>
         </DataProvider>
       </div>
     );
